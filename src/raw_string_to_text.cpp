0ef9af6cc150e2f35fd3f457b47339dc2975ca57
SamSamhuns <samhunsadamant@gmail.com>
2019-07-28 13:10:39 +0400

    Add cstring include in txt raw str converters

--- src/raw_string_to_text.cpp
+++ src/raw_string_to_text.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdio.h>
 #include <cstddef>
+#include <cstring>
 #include <iostream>
 #include <dirent.h>
 #include <unistd.h>
--- src/text_to_raw_string.cpp
+++ src/text_to_raw_string.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdio.h>
 #include <cstddef>
+#include <cstddef>
 #include <iostream>
 #include <dirent.h>
 #include <unistd.h>
