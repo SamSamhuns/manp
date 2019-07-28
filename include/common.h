e42b55d5cc825533a7778aab5f7523d9e5db7717
SamSamhuns <samhunsadamant@gmail.com>
2019-07-28 12:57:49 +0400

    Change delim and add all txt files in include

--- include/common.h
+++ include/common.h
@@ -4,7 +4,7 @@
 #define DEBUG 1  // Debug Mode, 1 = On, - = Off
 #define START_DELIM "R\"==~~~~==("   // delimits start of raw string
 #define END_DELIM ")==~~~~==\""      // delimits end of raw string
-#define CPP_MAP_KEY_VALUE_DELIM "CPP==map==key==delimiter"  // splits keys from values
-#define CPP_MAP_MODULE_DELIM "CPP==new==module==delimiter"  // splits different modules
+#define CPP_MAP_KEY_VALUE_DELIM "~~CPP==map==key==delim~~"  // splits keys from values
+#define CPP_MAP_MODULE_DELIM "~~CPP==new==module==delim~~"  // splits different modules
 
 #endif
